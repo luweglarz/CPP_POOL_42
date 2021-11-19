c3f00567f7f560830395e0904165b33a2293225e
RolandGamos <lucasweglarz9@gmail.com>
2021-11-19 18:14:59 +0100

    CPP_08 ex02 little changes in main and operator

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -7,9 +7,9 @@ int main()
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
+    std::cout << "Mastack top value: " << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    std::cout << "Mstack size: " << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
--- CPP_08/ex02/mutantstack.cpp
+++ CPP_08/ex02/mutantstack.cpp
@@ -2,10 +2,20 @@
 
 template <typename T>
 MutantStack<T>::MutantStack(): std::stack<T>(){
-
 }
 
 template <typename T>
 MutantStack<T>::~MutantStack(){
+}
 
+template <typename T>
+
+MutantStack<T>::MutantStack(const MutantStack &src): std::stack<T>(src){
+    *this = src;
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &src){
+    std::stack<T>::operator=(src);
+    return (*this);
 }
